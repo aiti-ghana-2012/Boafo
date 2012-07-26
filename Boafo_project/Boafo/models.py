2ed4b255237b32aa968849154bc16eb26f5572ff
Jspawn <jspawn@Jspawn-PC.(none)>
2012-07-26 10:02:11 +0100

    working admin side

--- Boafo_project/Boafo/models.py
+++ Boafo_project/Boafo/models.py
@@ -7,6 +7,23 @@ from django.contrib import admin
 class Location (models.Model):
     location = models.CharField(max_length = 255)               # various locations in the system. service providers
                                                                                              # will choose a location from a drop down menu
+    def __unicode__(self):
+        return self.location
+
+#       CREATING THE MODEL Category
+class Category (models.Model):
+    service_category = models.CharField(max_length = 255)    # 
+    description = models.TextField()
+    def __unicode__(self):
+        return self.service_category
+
+
+#       CREATING THE MODEL Service
+class Service(models.Model):                                                    
+    service = models.CharField(max_length = 255)                   # The name of the service
+    category = models.ForeignKey(Category)                            # this maps a service to a category
+    def __unicode__(self):
+        return self.service
 
 #       CREATING THE MODEL ServiceProvider
 class ServiceProvider(models.Model):
@@ -14,18 +31,11 @@ class ServiceProvider(models.Model):
     personnel_name = models.CharField(max_length = 255)     #name of personnel at organisation
     telephone  = models.CharField(max_length =  20)               # service provider's telephone
     email = models.EmailField()                                                  # service provider's email
-    service = models.ForeignKey(Location)                                # kind of service provided by service provider
+    service = models.ForeignKey(Service)                                # kind of service provided by service provider
     location = models.ForeignKey(Location)                               # service provider's location
+    def __unicode__(self):
+        return self.organization_name
 
-#       CREATING THE MODEL Service
-class Service(models.Model):                                                    
-    service = models.CharField(max_length = 255)                   # The name of the service
-    category = models.ForeignKey(Category)                            # this maps a service to a category 
-
-#       CREATING THE MODEL Category
-class Category (models.Model):
-    service_category = models.CharField(max_length = 255)    # 
-    description = models.TextField()
 
 #       CREATING THE MODEL Subscription
 class Subscription (models.Model):
@@ -36,6 +46,25 @@ class Subscription (models.Model):
                                                                                                 # details are sent to a customer
     organization = models.ForeignKey(ServiceProvider)             # the subscription detail is linked to the service provider on the
                                                                                                 # primary key of the service provider
+    def __unicode__(self):
+        return self.subscription_tracker
+
+
+##class CommentInline(admin.TabularInline):
+##        model = Comment
+##
+##class PostAdmin(admin.ModelAdmin):
+##        list_display = ('title','created','updated')
+##        search_param = ('title','body')
+##        list_filter=('created','author')
+##        inlines = [CommentInline]
+##	
+##	
+##class CommentAdmin(admin.ModelAdmin):
+##        list_display = ('post','author','fscb','created','updated')
+##        search_param = ('created','author')
+##admin.site.register(Post, PostAdmin)
+##admin.site.register(Comment, CommentAdmin)
     
 admin.site.register(Location)
 admin.site.register(ServiceProvider)
---
+++ Boafo_project/Boafo/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+##    url(r'^$', 'blog.views.home'),
+##    url(r'^posts/$', 'blog.views.post_list'),
+##    url(r'^comments/(?P<id>\d+)/\w+?$', 'blog.views.edit_comment'),
+##    url(r'^posts/(?P<id>\d+)/((?P<showComments>.*)/)?$', 'blog.views.post_detail'),
+   
+    ## add your url here
+##    url(r'^posts/search/(?P<text>\w+)/$','blog.views.post_search'),
+)
